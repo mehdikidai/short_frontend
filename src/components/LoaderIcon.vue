7538d4b45e24b143bb662770b0d7e5b9099a10a9
mehdi_kidai <mehdikidai@gmail.com>
2024-09-21 01:11:17 +0100

    aaa

---
+++ src/components/LoaderIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <Icon name="progress_activity"/>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+i {
+    font-size: 16px;
+    animation: rotation 1s linear infinite ;
+}
+
+@keyframes rotation {
+    0% {
+       transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
---
+++ src/components/Tit.vue
@@ -0,0 +1,32 @@
+<template>
+    <h1><Icon :name="icon" />{{ text }}</h1>
+</template>
+
+<script setup>
+defineProps({
+    text: {
+        type: String,
+        default: "hhhhhh",
+    },
+    icon:{
+        type: String,
+        default: "home",
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    font-size: 20px;
+    font-weight: 600;
+    color: var(--black);
+    margin-block: 20px;
+    text-transform: capitalize;
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    i {
+        opacity: 0.7;
+    }
+}
+</style>
