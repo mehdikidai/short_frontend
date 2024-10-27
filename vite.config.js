1006e419cdc12d16110de18cfed7b2a1d7f93e7f
mehdikidai <mehdikidai@gmail.com>
2024-10-27 19:55:55 +0000

    add base in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [
         vue(),
         AutoImport({
